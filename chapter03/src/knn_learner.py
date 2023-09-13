50a24f892e2031f4d48487b271ff8fa6a4af4593
MaxEntroy <likang@tju.edu.cn>
2023-09-13 14:25:49 +0800

    chapter03: Update knn_learner.

--- chapter03/src/knn_learner.py
+++ chapter03/src/knn_learner.py
@@ -2,17 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# 读入mnist数据集
-m_x = np.loadtxt('mnist_x.txt', delimiter=' ')
-m_y = np.loadtxt('mnist_y.txt')
+def load_samples_and_labels():
+    # load raw samples and labels
+    samples = np.loadtxt('mnist_x.txt', delimiter=' ')
+    labels = np.loadtxt('mnist_y.txt')
 
-print (m_x)
+    # shuffle raw samples and labels
+    np.random.seed(0)
+    shuffle_indices = np.random.permutation(np.arange(len(samples)))
+    samples = samples[shuffle_indices]
+    labels = labels[shuffle_indices]
 
-# 数据集可视化
-data = np.reshape(np.array(m_x[0], dtype=int), [28, 28])
-plt.figure()
+    # make training_set and test_set
+    ratio = 0.8
+    split = int(len(samples) * ratio)
+    samples_train_set, samples_test_set = samples[:split], samples[split:]
+    labels_train_set, labels_test_set = labels[:split], labels[split:]
+    return samples_train_set, samples_test_set, labels_train_set, labels_test_set
 
-plt.imshow(data, cmap='gray')
-plt.show()
+def distance(a, b):
+    return np.sqrt(np.sum(np.square(a - b)))
 
-print (data)
