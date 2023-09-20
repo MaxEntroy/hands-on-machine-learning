86cdc865a1193c037cd9a56ecd7b57d8fc95229a
MaxEntroy <likang@tju.edu.cn>
2023-09-20 21:40:06 +0800

    chapter03: Update knn_learner.

--- chapter03/README.md
+++ chapter03/README.md
@@ -4,4 +4,15 @@
 
 ### 实践
 
-对于samples数据的处理，本质二维的0-1 数表，实际存储的时候，拍成了一维的结构，这点注意。
+- 对于samples数据的处理，本质二维的0-1 数表，实际存储的时候，拍成了一维的结构，这点注意。
+- 具体的数据集合，1000 个样本
+    - 每一个 X，原本是28 * 28的矩阵，拍成一维矩阵后是 784 个元素
+    - 每一个 Y 就是一个具体的图像值
+
+```python
+np.random.seed(0)
+shuffle_indices = np.random.permutation(np.arange(len(samples)))
+samples = samples[shuffle_indices]
+labels = labels[shuffle_indices]
+```
+shuffle的逻辑，python写法很简单。最后的赋值类似a[0] = a[9]
--- chapter03/src/knn_learner.py
+++ chapter03/src/knn_learner.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+def show_a_sample(sample):
+    data = np.reshape(np.array(sample, dtype=int), [28, 28])
+    plt.figure()
+    plt.imshow(data, cmap='Blues')
+    plt.show()
+    
 def load_samples_and_labels():
     # load raw samples and labels
     samples = np.loadtxt('mnist_x.txt', delimiter=' ')
@@ -19,6 +25,8 @@ def load_samples_and_labels():
     samples_train_set, samples_test_set = samples[:split], samples[split:]
     labels_train_set, labels_test_set = labels[:split], labels[split:]
     return samples_train_set, samples_test_set, labels_train_set, labels_test_set
+
+load_samples_and_labels()
 
 def distance(a, b):
     return np.sqrt(np.sum(np.square(a - b)))
